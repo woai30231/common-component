fe2a00260d721498e591890d0c6934318830fc4c
Jianfeng <554830231@qq.com>
2017-04-14 17:08:03 +0800

    add contents

---
+++ 4/index.js
@@ -0,0 +1,156 @@
+var interface_data_arr = [
+    {
+        "id": 4,
+        "model_name": "大小",
+        "sortIndex": "255",
+        "attr_value": [
+            {
+                "is_active": false,
+                "alternative_name": "XL",
+                "id": 3
+            },
+            {
+                "is_active": true,
+                "alternative_name": "L",
+                "id": 5
+            },
+            {
+                "is_active": false,
+                "alternative_name": "M",
+                "id": 7
+            },
+            {
+                "is_active": false,
+                "alternative_name": "S",
+                "id": 15
+            }
+        ],
+        "is_currentUse": true
+    },
+    {
+        "id": 5,
+        "model_name": "颜色",
+        "sortIndex": "256",
+        "attr_value": [
+            {
+                "is_active": true,
+                "alternative_name": "红色",
+                "id": 33
+            },
+            {
+                "is_active": false,
+                "alternative_name": "黑色",
+                "id": 34
+            },
+            {
+                "is_active": false,
+                "alternative_name": "蓝色",
+                "id": 35
+            },
+            {
+                "is_active": false,
+                "alternative_name": "绿色",
+                "id": 36
+            },
+            {
+                "is_active": false,
+                "alternative_name": "黄色",
+                "id": 37
+            }
+        ],
+        "is_currentUse": true
+    },
+    {
+        "id": 6,
+        "model_name": "对象",
+        "sortIndex": "257",
+        "attr_value": [
+            {
+                "is_active": false,
+                "alternative_name": "成人",
+                "id": 83
+            },
+            {
+                "is_active": false,
+                "alternative_name": "儿童男",
+                "id": 97
+            },
+            {
+                "is_active": false,
+                "alternative_name": "儿童女",
+                "id": 98
+            },
+            {
+                "is_active": false,
+                "alternative_name": "绿色",
+                "id": 36
+            },
+            {
+                "is_active": false,
+                "alternative_name": "黄色",
+                "id": 37
+            }
+        ],
+        "is_currentUse": false
+    },
+    {
+        "id": 7,
+        "model_name": "花色",
+        "sortIndex": "258",
+        "attr_value": [
+            {
+                "is_active": true,
+                "alternative_name": "红花",
+                "id": 100
+            },
+            {
+                "is_active": false,
+                "alternative_name": "百花",
+                "id": 120
+            },
+            {
+                "is_active": false,
+                "alternative_name": "粉花",
+                "id": 130
+            },
+            {
+                "is_active": false,
+                "alternative_name": "绿色",
+                "id": 36
+            },
+            {
+                "is_active": false,
+                "alternative_name": "黄色",
+                "id": 37
+            }
+        ],
+        "is_currentUse": true
+    }
+];
+
+var to_out_data = [
+        {
+            "skuStock": 120,
+            "price": 12.5,
+            "imgUrl": null,
+            "attrValues": "颜色3:黑色",
+            "attrIds": "1:1",
+            "goodsCode": "123"
+        },
+        {
+            "skuStock": 90,
+            "price": 12.5,
+            "imgUrl": null,
+            "attrValues": "颜色2:白色",
+            "attrIds": "1:2",
+            "goodsCode": "123"
+        },
+        {
+            "skuStock": 100,
+            "price": 12.5,
+            "imgUrl": null,
+            "attrValues": "颜色2:红色",
+            "attrIds": "1:5",
+            "goodsCode": "123"
+        }
+    ];
