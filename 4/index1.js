cace769c457d653f1106893a4ba3ddc7d334113f
Jianfeng <554830231@qq.com>
2017-04-20 13:38:36 +0800

    修改文件

---
+++ 4/index1.js
@@ -0,0 +1,101 @@
+var table = [
+        {
+            "id": 259,
+          "skuStock": 120,
+          "price": 12.5,
+          "imgUrl": null,
+          "attrValues": "颜色:黑色",
+          "attrIds": "1:1",
+          "goodsCode": "123"
+        },
+        {
+            "id": 260,
+          "skuStock": 90,
+          "price": 12.5,
+          "imgUrl": null,
+          "attrValues": "颜色:白色",
+          "attrIds": "1:2",
+          "goodsCode": "123"
+        },
+        {
+            "id": 261,
+          "skuStock": 100,
+          "price": 12.5,
+          "imgUrl": null,
+          "attrValues": "颜色:红色",
+          "attrIds": "1:5",
+          "goodsCode": "123"
+        }
+       ];
+        
+
+
+
+     var interface_data_arr =[
+    {
+        "id": 4,
+        "model_name": "大小",
+        "sortIndex": "255",
+        "attr_value": [
+            {
+                "is_active": false,
+                "alternative_name": "XL",
+                "id": 3
+            },
+            {
+                "is_active": true,
+                "alternative_name": "L",
+                "id": 5
+            },
+            {
+                "is_active": false,
+                "alternative_name": "M",
+                "id": 7
+            },
+            {
+                "is_active": false,
+                "alternative_name": "S",
+                "id": 15
+            }
+        ],
+        "is_currentUse": false
+    },
+    {
+        "id": 1,
+        "model_name": "颜色",
+        "sortIndex": "256",
+        "attr_value": [
+            {
+                "is_active": true,
+                "alternative_name": "红色",
+                "id": 5
+            },
+            {
+                "is_active": true,
+                "alternative_name": "黑色",
+                "id": 1
+            },
+            {
+                "is_active": false,
+                "alternative_name": "蓝色",
+                "id": 35
+            },
+            {
+                "is_active": false,
+                "alternative_name": "绿色",
+                "id": 36
+            },
+            {
+                "is_active": false,
+                "alternative_name": "黄色",
+                "id": 37
+            },
+            {
+                "is_active": true,
+                "alternative_name": "白色",
+                "id": 2
+            }
+        ],
+        "is_currentUse": true
+    }
+   ];
